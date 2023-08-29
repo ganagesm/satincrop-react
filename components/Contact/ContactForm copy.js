a264d523ed23f403aeb1c8552ddf8eeac811bf5a
Sopan Ganage <gangesm1@gmail.com>
2023-08-29 09:15:38 +0530

    removed sendgrid api

--- components/Contact/ContactForm copy.js
+++ components/Contact/ContactForm copy.js
@@ -5,8 +5,8 @@ import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 import baseUrl from "../../utils/baseUrl";
 
-const sendGridMail = require('@sendgrid/mail');
-sendGridMail.setApiKey('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');
+// const sendGridMail = require('@sendgrid/mail');
+// sendGridMail.setApiKey('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');
 
 const alertContent = () => {
   MySwal.fire({
