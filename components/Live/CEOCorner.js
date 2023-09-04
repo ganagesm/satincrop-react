4eb2882ed1613d85157db990728fb66cb242e58e
Sopan Ganage <gangesm1@gmail.com>
2023-09-04 22:21:16 +0530

    Update ceocorner.js

--- components/Live/CEOCorner.js
+++ components/Live/CEOCorner.js
@@ -54,37 +54,7 @@ const BlogPostSlider = () => {
               </div>
             </div>
             <div className="col-lg-8 col-sm-12">
-            <SwiperSlide>
-                    <div className="single-blog-post">
-                      <div className="entry-thumbnail">
-                        <Link href="/embracing-machine-first-approach/">
-                          <img src="/images/engineer-cooperation.jpg" alt="image" />
-                        </Link>
-                      </div>
-
-                      <div className="entry-post-content">
-                        <div className="entry-meta">
-                          <ul>
-                            <li>Sep 04, 2023</li>
-                          </ul>
-                        </div>
-
-                        <h3>
-                          <Link href="/embracing-machine-first-approach/">
-                          Embracing the Machine-First Approach to Forge Ahead
-                          </Link>
-                        </h3>
-
-                        <p>
-                        In our relentless pursuit of pushing boundaries....
-                        </p>
-
-                        <Link href="/embracing-machine-first-approach/" className="learn-more-btn">
-                          Read More <i className="flaticon-add"></i>
-                        </Link>
-                      </div>
-                    </div>
-                  </SwiperSlide>
+              
               <Swiper
                 spaceBetween={20}
                 autoplay={{
@@ -110,6 +80,37 @@ const BlogPostSlider = () => {
                 className="partner-slides"
               >
                 <div className="row justify-content-center">
+                <SwiperSlide>
+                <div className="single-blog-post">
+                  <div className="entry-thumbnail">
+                    <Link href="/embracing-machine-first-approach/">
+                      <img src="/images/engineer-cooperation.jpg" alt="image" />
+                    </Link>
+                  </div>
+
+                  <div className="entry-post-content">
+                    <div className="entry-meta">
+                      <ul>
+                        <li>Sep 04, 2023</li>
+                      </ul>
+                    </div>
+
+                    <h3>
+                      <Link href="/embracing-machine-first-approach/">
+                        Embracing the Machine-First Approach to Forge Ahead
+                      </Link>
+                    </h3>
+
+                    <p>
+                      In our relentless pursuit of pushing boundaries....
+                    </p>
+
+                    <Link href="/embracing-machine-first-approach/" className="learn-more-btn">
+                      Read More <i className="flaticon-add"></i>
+                    </Link>
+                  </div>
+                </div>
+              </SwiperSlide>
                   <SwiperSlide>
                     <div className="single-blog-post">
                       <div className="entry-thumbnail">
