958b15f67e5c42e239dd9bbbd68589ede94ee23c
Sopan Ganage <gangesm1@gmail.com>
2023-09-06 21:47:20 +0530

    Update baseurl.js

--- pages/_document.js
+++ pages/_document.js
@@ -22,23 +22,6 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/favicon.png"></link>
           <link rel="icon" type="image/png" href="/180x180.png"></link>
           {/* <link rel="icon" type="image/png" href="/outsource.svg"></link> */}
-          <Script id="google-analytics">
-            {`
-          window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-          ga('create', 'UA-228721401-1', 'auto');
-          ga('send', 'pageview');
-        `}
-          </Script>
-          <Script src="https://www.google-analytics.com/analytics.js" />
-          <Script id="google-tagmanager">
-            {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TVV382K');
-        `}
-          </Script>
         </Head>
         <body>
           <Main />
