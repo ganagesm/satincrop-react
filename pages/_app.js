8139d4d58c2fd26b0f125be80b3a6f0959b30a91
Sopan Ganage <gangesm1@gmail.com>
2023-09-06 20:53:39 +0530

    add googletagmanager.js

--- pages/_app.js
+++ pages/_app.js
@@ -18,17 +18,10 @@ import "../styles/responsive.css";
 import Head from "next/head";
 import GoTop from "../components/Shared/GoTop";
 
-// Initialize Google Analytics
-
-ReactGA.initialize('UA-91820466-2');
-
 function MyApp({ Component, pageProps }) {
 
   React.useEffect(() => {
     AOS.init();
-    // Track pageview using Google Analytics
-    // ReactGA.pageview(window.location.pathname + window.location.search);
-    ReactGA.pageview('/');
   }, []);
   return (
     <>
@@ -50,7 +43,7 @@ function MyApp({ Component, pageProps }) {
         {/* <!-- Google tag (gtag.js) --> */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=UA-66413880-1"></Script>
         <Script>
-        {`
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -58,7 +51,7 @@ function MyApp({ Component, pageProps }) {
           `}
         </Script>
         <Script id="google-analytics">
-        {`
+          {`
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -67,10 +60,19 @@ function MyApp({ Component, pageProps }) {
           ga('create', 'UA-228721401-1', 'auto');
           ga('send', 'pageview');
         `}
-      </Script>
+        </Script>
+        <Script>
+          {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); )(window,document,'script','dataLayer','GTM-M73DP9H');
+          `}</Script>
       </Head>
 
       <Component {...pageProps} />
+
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M73DP9H"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
 
       {/* Go Top Button */}
       <GoTop scrollStepInPx="50" delayInMs="10.50" />
