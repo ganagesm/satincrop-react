15751c7c7110a3501bd08f081b6cd170339b0353
Sopan Ganage <gangesm1@gmail.com>
2023-09-06 21:55:12 +0530

    update G-EFJWZ4P8SR

--- pages/_app.js
+++ pages/_app.js
@@ -32,12 +32,10 @@ function MyApp({ Component, pageProps }) {
 
       <Script strategy="lazyOnload">
         {`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-EFJWZ4P8SR'); {
-                    page_path: window.location.pathname,
-                    });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-EFJWZ4P8SR');
                 `}
       </Script>
       <Head>
