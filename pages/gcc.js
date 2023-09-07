d9c1a1477ea436af4982323a34327170db02564d
Sopan Ganage <gangesm1@gmail.com>
2023-09-07 21:11:16 +0530

    update zoho form

--- pages/gcc.js
+++ pages/gcc.js
@@ -12,6 +12,7 @@ import ContactForm from "../components/Contact/ContactForm";
 import Services from "../components/GCC/Services";
 import WhyChooseSAT from "../components/GCC/WhyChooseSAT";
 import BlogPostSlider from "../components/Live/BlogPostSlider";
+import ContactFormContent from "../components/Common/ContactFormContent";
 import Head from 'next/head';
 import { Component } from "react";
 import Link from "next/link";
@@ -158,27 +159,7 @@ const Index = () => {
       {/* <CaseStudiesSlider /> */}
       <BlogPostSlider />
       {/* <Testimonials /> */}
-      <section className="contact-area ptb-100">
-        <div className="container">
-          <div className="section-title">
-            <h3>Reach out to us today to explore how our comprehensive solution can pave the way for your global success.</h3>
-          </div>
-
-          <div className="contact-form">
-            <div className="row align-items-center">
-              <div className="col-lg-5 col-md-12">
-                <div className="contact-image">
-                  <img src="/images/footer-img.png" alt="image" />
-                </div>
-              </div>
-
-              <div className="col-lg-7 col-md-12">
-                <ContactForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <ContactFormContent />
       <Footer />
     </>
   )
