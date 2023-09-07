46d08d125a4150aac06a3f46729bb54ba0722edf
Sopan Ganage <gangesm1@gmail.com>
2023-09-07 22:31:27 +0530

    Upadate gcc

--- pages/articles/gcc-evolution.js
+++ pages/articles/gcc-evolution.js
@@ -11,13 +11,13 @@ const BlogDetails = () => {
     <>
       <Navbar />
 
-       <ArticleBanner
+      <ArticleBanner
         pageTitle="GCC Evolution: From IT Support to Tech Innovation Hubs"
         homePageUrl="/"
         homePageText="Home"
         activePageText="Articles"
         bgImgClass="item-bg3"
-      /> 
+      />
       {/* <BlogDetailsContentChoosingRightAITrainedNearshore /> */}
       <section className="blog-details-area ptb-50">
         <div className="container">
@@ -32,49 +32,49 @@ const BlogDetails = () => {
                        Over the years, they have evolved significantly, transforming from mere support centers to 
                        becoming key drivers of innovation and efficiency. In this blog, we'll delve into the world 
                        of GCCs and how SA Technologies' GCCs are revolutionizing business operations. </p> */}
-                   
+
                   </div>
 
                   {/* <div className="article-image" style={{marginTop:"10px",marginBottom:"10px",}}>
                     <img src="/images/blog/choosingRightAITrainedNearshore-min.png" alt="image" />
                   </div> */}
 
-                    <h3>What Are Global Capability Centers? </h3>
-                    <p>Global Capability Centers, often referred to as GCCs or simply captives, are offshore or nearshore entities that organizations establish to perform specific business functions. These centers operate as extensions of the parent company, providing a range of services that can include IT support, research and development, finance and accounting, customer service, and more.  </p>
-                    
+                  <h3>What Are Global Capability Centers? </h3>
+                  <p>Global Capability Centers, often referred to as GCCs or simply captives, are offshore or nearshore entities that organizations establish to perform specific business functions. These centers operate as extensions of the parent company, providing a range of services that can include IT support, research and development, finance and accounting, customer service, and more.  </p>
 
-                    <h3>The Evolution of GCCs </h3>
-                    <p><b>Phase 1:</b> Cost-Centric Operations- initially, GCCs were primarily set up as cost-saving measures. Companies leveraged labor arbitrage by establishing centers in countries with lower labor costs. The primary focus was on reducing operational expenses while maintaining service quality. </p>
-                    
 
-                   
-                    <p><b>Phase 2:</b> Expanding Services- As GCCs matured, organizations began expanding the scope of services offered. This involved taking on more complex tasks, such as research and development, data analytics, and advanced IT functions. Companies recognized the potential for GCCs to contribute significantly to innovation and growth.  </p>
-                    
-                    <p><b>Phase 3:</b> Phase 3: Becoming Innovation Hubs- Today, SA Technologies' GCCs have evolved into innovation hubs. They are at the forefront of technological advancements, driving digital transformation, and enabling businesses to stay competitive in the digital age. GCCs are no longer just cost-saving centers; they are strategic assets for organizations. </p>
+                  <h3>The Evolution of GCCs </h3>
+                  <p><b>Phase 1:</b> Cost-Centric Operations- initially, GCCs were primarily set up as cost-saving measures. Companies leveraged labor arbitrage by establishing centers in countries with lower labor costs. The primary focus was on reducing operational expenses while maintaining service quality. </p>
 
-                    <div className="article-image" style={{marginTop:"10px",marginBottom:"10px",}}>
-                      <img src="/images/blog/evaluation.jpg" alt="image" />
-                    </div>   
 
-                     <h3>The Role of GCCs in Today's Business Landscape </h3>
-                    <p><b>Innovation:</b> SA Technologies' GCCs are centers of innovation, working on cutting-edge technologies like artificial intelligence, machine learning, and blockchain. They develop solutions that drive business growth and enhance customer experiences.</p>
-                    
-                    <p><b>Efficiency:</b> By leveraging SA Technologies' GCCs, companies can streamline their operations, improve processes, and optimize costs. This leads to greater operational efficiency and competitiveness. </p>
-                    
-                    <p><b>Access to Talent:</b>   SA Technologies' GCCs provide access to a global talent pool. Organizations can tap into diverse skill sets and expertise, allowing them to tackle complex projects and challenges.</p>
-                    
-                    <p><b>Risk Mitigation:</b> SA Technologies' GCCs offer risk mitigation benefits, such as geographic diversification. They provide a buffer against unforeseen disruptions by spreading operations across different regions.</p>
-                    
-                    <p><b>calability:</b> SA Technologies' GCCs allow for scalable operations. As businesses expand, they can easily scale up their GCC teams to support their growth.</p>
-                    
-                    <h3>Conclusion </h3>
-                    <p> The evolution of Global Capability Centers from cost-centric operations to innovation hubs has been remarkable. Today, SA Technologies' GCCs play a pivotal role in driving growth, fostering innovation, and ensuring operational excellence for organizations across various industries. As businesses continue to navigate an increasingly competitive landscape, SA Technologies' GCCs will remain indispensable partners in their quest for success. </p>
-                    
+
+                  <p><b>Phase 2:</b> Expanding Services- As GCCs matured, organizations began expanding the scope of services offered. This involved taking on more complex tasks, such as research and development, data analytics, and advanced IT functions. Companies recognized the potential for GCCs to contribute significantly to innovation and growth.  </p>
+
+                  <p><b>Phase 3:</b> Phase 3: Becoming Innovation Hubs- Today, SA Technologies' GCCs have evolved into innovation hubs. They are at the forefront of technological advancements, driving digital transformation, and enabling businesses to stay competitive in the digital age. GCCs are no longer just cost-saving centers; they are strategic assets for organizations. </p>
+
+                  <div className="article-image" style={{ marginTop: "10px", marginBottom: "10px", }}>
+                    <img src="/images/blog/evaluation.jpg" alt="image" />
+                  </div>
+
+                  <h3>The Role of GCCs in Today's Business Landscape </h3>
+                  <p><b>Innovation:</b> SA Technologies' GCCs are centers of innovation, working on cutting-edge technologies like artificial intelligence, machine learning, and blockchain. They develop solutions that drive business growth and enhance customer experiences.</p>
+
+                  <p><b>Efficiency:</b> By leveraging SA Technologies' GCCs, companies can streamline their operations, improve processes, and optimize costs. This leads to greater operational efficiency and competitiveness. </p>
+
+                  <p><b>Access to Talent:</b>   SA Technologies' GCCs provide access to a global talent pool. Organizations can tap into diverse skill sets and expertise, allowing them to tackle complex projects and challenges.</p>
+
+                  <p><b>Risk Mitigation:</b> SA Technologies' GCCs offer risk mitigation benefits, such as geographic diversification. They provide a buffer against unforeseen disruptions by spreading operations across different regions.</p>
+
+                  <p><b>calability:</b> SA Technologies' GCCs allow for scalable operations. As businesses expand, they can easily scale up their GCC teams to support their growth.</p>
+
+                  <h3>Conclusion </h3>
+                  <p> The evolution of Global Capability Centers from cost-centric operations to innovation hubs has been remarkable. Today, SA Technologies' GCCs play a pivotal role in driving growth, fostering innovation, and ensuring operational excellence for organizations across various industries. As businesses continue to navigate an increasingly competitive landscape, SA Technologies' GCCs will remain indispensable partners in their quest for success. </p>
+
 
                 </div>
 
               </div>
-                       
+
 
               {/* Blog Comments Area */}
               {/* <BlogCommentsArea /> */}
@@ -88,19 +88,23 @@ const BlogDetails = () => {
 
         <div className="GCCJourney">
           <div className="container">
-          <div className="row ptb-50 align-items-center">
-            <div className="col-lg-6 col-md-6">
-              <h2 className="align-items-center">About the Author
-              </h2>
+            <div className="row ptb-50 align-items-center">
+              <div className="col-lg-3 col-md-6">
+                <h2 className="align-items-center">About the Author </h2>
+                <div className="article-image" style={{ marginTop: "10px", marginBottom: "10px", }}>
+                  <img src="/images/team/amit-singh.jpeg" alt="image" />
+                  <h5 style={{ marginTop: "10px", marginBottom: "0", }} className="align-items-center">Amit Singh</h5>
+                  <p>VP Global delivery</p>
+                </div>
+              </div>
+              <div className="col-lg-7 col-md-6 offset-md-1">
+                <h2>Contact an SA Technologies Representative to Help You Build and Scale Global Teams</h2>
+                <Link href="/contact-us" className="btn btn-primary" style={{marginTop:"25px"}}>
+                  Contact Us
+                </Link>
+              </div>
+
             </div>
-            <div className="article-image" style={{marginTop:"10px",marginBottom:"10px",}}>
-              <img src="/images/team/amit.jpg" alt="image" />
-              <h5 style={{marginTop:"10px", marginBottom:"0",}} className="align-items-center">Amit Singh</h5>
-              <p>VP Global delivery</p>
-            </div>
-           
-           
-          </div>
           </div>
         </div>
 
