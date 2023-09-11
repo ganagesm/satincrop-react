5fb088811ea36303bcc2cf8b1ec06a0fbef48771
Sopan Ganage <gangesm1@gmail.com>
2023-09-11 20:59:15 +0530

    Create employee-verification

---
+++ pages/employee-verification.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Navbar from "../components/Live/Navbar";
+import OffshorePageBanner from "../components/Live/OffshorePageBanner";
+import RpoServiceDetailsContent from "../components/Live/RpoServiceDetailsContent";
+import RpoServices from "../components/Live/RpoServices";
+import Testimonials from "../components/Live/Testimonials";
+import Footer from "../components/Live/Footer";
+import Link from "next/link";
+import PartnerContent from "../components/Live/PartnerContent";
+import OurIndustry from "../components/Common/OurIndustry";
+import ContactForm from "../components/Contact/ContactForm";
+import BlogPostSlider from "../components/Live/BlogPostSlider";
+import ContactFormContent from "../components/Common/ContactFormContent";
+import Head from 'next/head';
+
+const ServiceDetails = () => {
+  return (
+    <>
+      <Head>
+        <title>Employee Verification | SA Technologies   </title>
+        <meta property="og:title" content="Employee Verification | SA Technologies  " key="title" />
+        <meta name="description" content="Employee Verification | SA Technologies" key="description" />
+      </Head>
+
+      <Navbar />
+      <OffshorePageBanner
+        pageTitle="Employee Verification"
+        homePageUrl="/"
+        homePageText="Home"
+        activePageText="Employee Verification"
+        bgImgClass="item-bg9"
+      />
+      <section className="blog-details-area ptb-110">
+        <div className="container">
+          <div className="row">
+
+            <div className="article-content">
+              <div className="entry-meta">
+                <p> Thanks for connecting with us for BGV verification of our ex-employee.</p>
+
+                <h3>Introduction</h3>
+                <p> Please note that it’s chargeable service from our side. Please pay using below “Pay Now” button & share the transaction details along with relevant documents of ex-employee to
+                  <a href="mailto:hr@dev1.satincorp.com">hr@dev1.satincorp.com</a> <a href="mailto:hr.ops2@dev1.satincorp.com">hr.ops2@dev1.satincorp.com</a> <a href="matilto:pck@dev1.satincorp.com">pck@dev1.satincorp.com</a>  .  </p>
+                <Link href="https://pages.razorpay.com/pl_EqcKXF6O6IsTbd/view" target="_blank" className="btn btn-primary">
+                  Pay Now
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default ServiceDetails;
