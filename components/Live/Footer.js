6d03d9808cd4d3a52a624c997f6757e104d37158
Sopan Ganage <gangesm1@gmail.com>
2023-10-05 12:32:51 +0530

    Home page.

--- components/Live/Footer.js
+++ components/Live/Footer.js
@@ -100,7 +100,7 @@ const Footer = () => {
                     <Link href="/offshore">Blog</Link>
                   </li> */}
                   <li>
-                    <Link href="/careers">Join Our Team </Link>
+                    <Link href="/careers">Careers </Link>
                     {/* <Link href="https://jobs.satincorp.com/jobs/Careers" target="_blank">Careers</Link> */}
                   </li>
                   <li>
