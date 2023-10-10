b71b4ac1fe1115010630174beca6622d5b460232
Sopan Ganage <gangesm1@gmail.com>
2023-10-10 20:28:39 +0530

    i18n defaultLocale

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = {
     locales: ['en-US', 'es', 'fr', 'nl-NL'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    // defaultLocale: 'en-US',
+    defaultLocale: 'en-US',
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
     domains: [],
