3cb35408993e4faf1f33566919893e94aea28d9b
Sopan Ganage <gangesm1@gmail.com>
2023-10-10 20:25:57 +0530

    Update i18n

--- next.config.js
+++ next.config.js
@@ -10,15 +10,21 @@ const nextConfig = {
 }
 
 // module.exports = nextConfig
-
 module.exports = {
   i18n: {
-    locales: ['en-US', 'fr', 'nl-NL'],
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US', 'es', 'fr', 'nl-NL'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    // defaultLocale: 'en-US',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    domains: [],
   },
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
 };
