38d2b1d979b364bab6c769b0cbe83b4f8645ba30
Prashant Bhamare <prasant.bhamare@mindpooltech.net>
2023-11-02 18:03:00 +0530

    New GCP page layout design

---
+++ components/MicrosoftGC/TopFeaturedSolutions.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "next/link";
+
+const TopFeaturedSolutions = () => {
+  return (
+    <>
+      <div className="top-featured-area pt-50 pb-70">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-12 col-sm-12">
+              <div style={{textAlign:"center",}} className="top-featured-content">
+                <h2>Why SA Technologies?</h2>
+                <p>SA Technologies is a Google Gold Partner with 16+ years of experience. We are dedicated to simplifying and 
+                  streamlining highly specialized business processes for small and mid-sized businesses. We not only take you 
+                  through Rapid Implementation, but we closely analyze your steps to get your solution up and running as quickly 
+                  and efficiently as possible.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopFeaturedSolutions;
