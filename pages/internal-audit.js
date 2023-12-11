a427587f823af41fee420e4a06daaf1b360888f2
Prashant Bhamare <prasant.bhamare@mindpooltech.net>
2023-12-11 12:45:05 +0530

    internal audit page case studies hide

--- pages/internal-audit.js
+++ pages/internal-audit.js
@@ -31,7 +31,7 @@ const Index = () => {
       <WhyChooseSAT />
       <OurIndustry />
       {/* <WhyTriosource /> */}
-      <CaseStudiesSlider />
+      {/* <CaseStudiesSlider /> */}
       <BlogPostSlider />
       <Testimonials />
       <Footer />
