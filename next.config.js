724b3bf5d0400e586bd2727f96569e103f89f1e2
Sopan Ganage <gangesm1@gmail.com>
2023-12-31 14:45:44 +0530

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
-/** @type {import('next').NextConfig} */
-const path = require('path')
-const nextConfig = {
+const path = require('path');
+
+module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   optimizeFonts: false,
-}
-
-// module.exports = nextConfig
-
-module.exports = {
   webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
   },
 };
