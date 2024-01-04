4dd62c52a30f8e9b3797b6ff189a3b1b4b4f8277
Sopan Ganage <gangesm1@gmail.com>
2024-01-04 20:08:27 +0530

    update utils pages

--- utils/getPages.js
+++ utils/getPages.js
@@ -29,7 +29,9 @@ import fs from 'fs/promises';
 import path from 'path';
 
 export async function getPages() {
+  // const pagesDirectory = path.join(process.cwd(), 'pages');
   const pagesDirectory = path.join(process.cwd(), 'pages');
+
   const excludedPages = [
     'checkout', 'blog2', 'coming-soon', 'case-studies', 'blog-details', '_document', 'privacy-policy',
     'services', 'terms-conditions', 'customers-and-partners', 'index', 'cart', '_app', 'case-studies-details', 'partner'
