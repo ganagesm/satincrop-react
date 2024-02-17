4ccf37f54e3fae181039ef9b7c75c4dc22f5658c
Sopan Ganage <gangesm1@gmail.com>
2024-02-17 17:15:45 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -15,6 +15,21 @@ module.exports = {
   //     },
   //   ];
   // },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
   async redirects() {
     return [
       {
