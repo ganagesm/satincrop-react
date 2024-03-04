8208b5f799d4a030239564d855868fef0a9c9689
Sopan Ganage <gangesm1@gmail.com>
2024-03-04 21:27:20 +0530

    Update schma

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,9 @@
 // pages/blog/[slug].js
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import Head from 'next/head';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+import { ArticleJsonLd } from "next-seo";
 
 import Navbar from "../../components/Live/Navbar";
 import Footer from "../../components/Live/Footer";
@@ -12,7 +13,7 @@ export default function BlogPost() {
   const router = useRouter();
   const { slug } = router.query;
   const [post, setPost] = useState({});
-  const postApi = 'https://dev1.satincorp.com/wp-json/wp/v2/';
+  const postApi = "https://dev1.satincorp.com/wp-json/wp/v2/";
 
   useEffect(() => {
     async function fetchPost() {
@@ -20,18 +21,20 @@ export default function BlogPost() {
         const response = await fetch(`${postApi}posts?slug=${slug}`);
 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
+          console.log(post);
         }
 
         const postData = await response.json();
         setPost(postData[0]); // Assuming slug is unique, so we take the first item from the array
       } catch (error) {
-        console.error('Error fetching post:', error);
+        console.error("Error fetching post:", error);
       }
     }
 
     if (slug) {
       fetchPost();
+      console.log(post);
     }
   }, [slug]);
 
@@ -42,13 +45,71 @@ export default function BlogPost() {
 
   return (
     <>
-    <Head>
+      {/* <ArticleJsonLd
+        useAppDir={false}
+        url="https://example.com/article"
+        title="Article headline"
+        images={[
+          "https://example.com/photos/1x1/photo.jpg",
+          "https://example.com/photos/4x3/photo.jpg",
+          "https://example.com/photos/16x9/photo.jpg",
+        ]}
+        datePublished="2015-02-05T08:00:00+08:00"
+        dateModified="2015-02-05T09:00:00+08:00"
+        authorName={[
+          {
+            name: "Jane Blogs",
+            url: "https://example.com",
+          },
+          {
+            name: "Mary Stone",
+            url: "https://example.com",
+          },
+        ]}
+        publisherName="Gary Meehan"
+        publisherLogo="https://www.example.com/photos/logo.jpg"
+        description="This is a mighty good description of this article."
+        isAccessibleForFree={true}
+      /> */}
+      <ArticleJsonLd
+        type="BlogPosting"
+        url={`https://www.satincorp.com/blog/${post.slug}`}
+        title={post.title}
+        description={post.description}
+        datePublished={post.date_gmt}
+        dateModified={post.modified_gmt}
+        siteName={post.og_site_name}
+        publisherName="SA Technologies"
+        publisherLogo="https://www.satincorp.com/images/sa-logo.svg"
+        openGraph={{
+          type: "article",
+          url: `https://www.satincorp.com/blog/${post.slug}`,
+          article: {
+            publishedTime: post.datePublished,
+            author: {
+              type: "Organization",
+              name: "SA Technologies",
+            },
+          },
+        }}
+      />
+      <Head>
         <title>{post.yoast_head_json.og_title}</title>
-        <meta property="og:title" content={post.yoast_head_json.description} key="title" />
-        <meta name="description" content={post.yoast_head_json.description} key="" />
-        <meta property="og:image" content={post.yoast_head_json.og_image[0].url} />
+        <meta
+          property="og:title"
+          content={post.yoast_head_json.description}
+          key="title"
+        />
+        <meta
+          name="description"
+          content={post.yoast_head_json.description}
+          key=""
+        />
+        <meta
+          property="og:image"
+          content={post.yoast_head_json.og_image[0].url}
+        />
       </Head>
-
       <Navbar />
       <PageBanner
         pageTitle={post.title.rendered}
@@ -68,7 +129,9 @@ export default function BlogPost() {
                 <div className="article-content">
                   {/* Render individual blog post content here */}
                   <h1>{post.title.rendered}</h1>
-                  <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+                  <div
+                    dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+                  />
                 </div>
                 <div className="article-footer">
                   <div className="article-tags">
@@ -82,17 +145,32 @@ export default function BlogPost() {
                   <div className="article-share">
                     <ul className="social">
                       <li>
-                        <a href="https://www.facebook.com/satincorp" target="_blank" rel="noreferrer">
+                        <a
+                          href="https://www.facebook.com/satincorp"
+                          target="_blank"
+                          rel="noreferrer">
                           <i className="fab fa-facebook-f"></i>
                         </a>
                       </li>
                       <li>
-                        <a href="https://twitter.com/SatechGlobal" target="_blank" rel="noreferrer">
-                          <svg xmlns="http://www.w3.org/2000/svg" height="1em" fill="#212529" viewBox="0 0 512 512"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg>
+                        <a
+                          href="https://twitter.com/SatechGlobal"
+                          target="_blank"
+                          rel="noreferrer">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="1em"
+                            fill="#212529"
+                            viewBox="0 0 512 512">
+                            <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
+                          </svg>
                         </a>
                       </li>
                       <li>
-                        <a href="https://www.linkedin.com/company/sa-technologies-inc/" target="_blank" rel="noreferrer">
+                        <a
+                          href="https://www.linkedin.com/company/sa-technologies-inc/"
+                          target="_blank"
+                          rel="noreferrer">
                           <i className="fab fa-linkedin-in"></i>
                         </a>
                       </li>
