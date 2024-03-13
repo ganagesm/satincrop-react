5c643879ad904a9df995593bab7ea8bd9ee5de52
Sopan Ganage <gangesm1@gmail.com>
2024-03-13 20:40:37 +0530

    validator.schema

--- pages/index.js
+++ pages/index.js
@@ -17,10 +17,38 @@ import MainServices from "../components/Live/MainServices";
 import Script from "next/script";
 import Head from "next/head";
 import ZohoScript from "../components/ZohoScript";
+import { WebPageJsonLd, NextSeo, DefaultSeo } from "next-seo";
 
 const Index = () => {
   return (
     <>
+      <WebPageJsonLd
+        headline="SA Technologies | Leading IT Consulting: AI Solutions, Digital Engineering, Enterprise Modernization"
+        description="SA Technologies Your Trusted IT Consulting and Offshore Partner with 20 years of experience, serving 500 clients Elevate your business with AI Solutions, Digital Engineering, and Enterprise Modernization Services"
+        author="SA Technologies"
+        keywords="IT Consulting"
+        image="https://www.satincorp.com/images/about/5.jpg"
+      />
+      <NextSeo
+        openGraph={{
+          type: "website",
+          url: "https://www.satincorp.com/",
+          title:
+            "SA Technologies | Leading IT Consulting: AI Solutions, Digital Engineering, Enterprise Modernization",
+          description:
+            "SA Technologies Your Trusted IT Consulting and Offshore Partner with 20 years of experience, serving 500 clients Elevate your business with AI Solutions, Digital Engineering, and Enterprise Modernization Services",
+          images: [
+            {
+              url: "https://www.satincorp.com/images/about/5.jpg",
+              width: 800,
+              height: 600,
+              alt: "SA Technologies | Leading IT Consulting: AI Solutions, Digital Engineering, Enterprise Modernization",
+            },
+          ],
+          authorName: "SA Technologies",
+          keywords: "IT Consulting",
+        }}
+      />
       <Head>
         <title>
           SA Technologies | Leading IT Consulting: AI Solutions, Digital
