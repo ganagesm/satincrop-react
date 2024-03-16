389e7e195031567c4ae292703db59e6cb3f54b6a
Sopan Ganage <gangesm1@gmail.com>
2024-03-16 18:29:44 +0530

    Offer CGP page

---
+++ components/GCPOffer/TopFeaturedSolutions.js
@@ -0,0 +1,102 @@
+import React from "react";
+import Link from "next/link";
+
+const TopFeaturedSolutions = () => {
+  return (
+    <>
+      <section className="about-area ptb-50">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-12">
+              <div className="about-image">
+                <img src="/images/ppc-banner/why-google-2.jpeg" alt="image" />
+                <img src="/images/ppc-banner/why-google.jpeg" alt="image" />
+              </div>
+            </div>
+
+            <div className="col-lg-6 col-md-12">
+              <div className="about-content">
+                <h2>Why SA Technologies? </h2>
+                <p>
+                  {" "}
+                  SAT is a trusted Google Cloud partner that can help you get
+                  the most out of Google Cloud solutions. Here are just a few of
+                  the reasons why you should choose SA Technologies as your
+                  Google Cloud partner.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="about-inner-area">
+            <div className="row">
+              <div className="col-lg-4 col-md-6 col-sm-6">
+                <div className="about-text">
+                  <h3>Rapid implementation</h3>
+                  <p>
+                    SA Technologies has a proven track record of success in
+                    helping customers quickly implement Google Cloud solutions.
+                    They understand the unique challenges that businesses face,
+                    and they work closely with their customers to develop a
+                    customized implementation plan that meets their specific
+                    needs.
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-6">
+                <div className="about-text">
+                  <h3>Expertise</h3>
+                  <p>
+                    SA Technologies' team of Google Cloud certified engineers
+                    has the expertise and experience to help businesses get the
+                    most out of Google Cloud solutions. They can help businesses
+                    choose the right solutions for their needs, and they can
+                    help them implement and manage those solutions effectively.
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+                <div className="about-text">
+                  <h3>Support</h3>
+                  <p>
+                    SA Technologies provides comprehensive support to their
+                    customers throughout the implementation process and beyond.
+                    They offer a variety of support options, including training,
+                    consulting, and 24/7 technical support.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Shape Images */}
+        <div className="shape-img1">
+          <img src="/images/shape/shape1.png" alt="image" />
+        </div>
+        <div className="shape-img2">
+          <img src="/images/shape/shape2.svg" alt="image" />
+        </div>
+        <div className="shape-img3">
+          <img src="/images/shape/shape3.png" alt="image" />
+        </div>
+        <div className="shape-img4">
+          <img src="/images/shape/shape4.svg" alt="image" />
+        </div>
+        <div className="shape-img5">
+          <img src="/images/shape/shape5.svg" alt="image" />
+        </div>
+        <div className="shape-img6">
+          <img src="/images/shape/shape6.png" alt="image" />
+        </div>
+        <div className="dot-shape1">
+          <img src="/images/shape/dot1.png" alt="image" />
+        </div>
+        <div className="dot-shape2">
+          <img src="/images/shape/dot2.png" alt="image" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TopFeaturedSolutions;
---
+++ pages/gcp-credit-offer.js
@@ -0,0 +1,345 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import AboutAI from "../components/GCPOffer/AboutAI";
+import TopFeaturedSolutions from "../components/GCPOffer/TopFeaturedSolutions";
+import CustomerSuccess from "../components/GCPOffer/CustomerSuccess";
+
+const ContactFormContent = () => {
+  const executeFunction = () => {
+    try {
+      var f = document.createElement("iframe");
+      f.src =
+        "https://forms.zohopublic.in/satechsoftwareipvtltd/form/Commonform/formperma/OqtO4dMucPCkviSHfWfmtFFImjjtai4d-o1gh8xkwX0?zf_rszfm=1";
+      f.style.border = "none";
+      f.style.height = "982px";
+      f.style.width = "90%";
+      f.style.transition = "all 0.5s ease";
+
+      var d = document.getElementById(
+        "zf_div_OqtO4dMucPCkviSHfWfmtFFImjjtai4d-o1gh8xkwX0"
+      );
+      d.appendChild(f);
+      window.addEventListener(
+        "message",
+        function () {
+          var evntData = event.data;
+          if (evntData && evntData.constructor == String) {
+            var zf_ifrm_data = evntData.split("|");
+            if (zf_ifrm_data.length == 2) {
+              var zf_perma = zf_ifrm_data[0];
+              var zf_ifrm_ht_nw = parseInt(zf_ifrm_data[1], 10) + 15 + "px";
+              var iframe = document
+                .getElementById(
+                  "zf_div_OqtO4dMucPCkviSHfWfmtFFImjjtai4d-o1gh8xkwX0"
+                )
+                .getElementsByTagName("iframe")[0];
+              if (
+                iframe.src.indexOf("formperma") > 0 &&
+                iframe.src.indexOf(zf_perma) > 0
+              ) {
+                var prevIframeHeight = iframe.style.height;
+                if (prevIframeHeight != zf_ifrm_ht_nw) {
+                  iframe.style.height = zf_ifrm_ht_nw;
+                }
+              }
+            }
+          }
+        },
+        false
+      );
+    } catch (e) {}
+  };
+  //Zoho Sales Iq Script:
+  const useScript = (url, widgetCode) => {
+    useEffect(() => {
+      executeFunction();
+      const script = document.createElement("script");
+      script.setAttribute("type", "text/javascript");
+
+      let code = `var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode: "${widgetCode}", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="${url}";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.innerHTML = "<div id='zsiqwidget'></div>";`;
+
+      script.appendChild(document.createTextNode(code));
+      document.body.appendChild(script);
+
+      return () => {
+        document.body.removeChild(script);
+      };
+    }, [url]);
+  };
+  return (
+    <>
+      <Head>
+        <title>
+          Effectively Collaborate with Remote Teams Using Google Workspace{" "}
+        </title>
+        <meta
+          property="og:title"
+          content="Effectively Collaborate with Remote Teams Using Google Workspace "
+          key="title"
+        />
+        <meta
+          name="description"
+          content="Experience accelerated app development with SA Technologies Mendix services. Empower your business with innovative, low-code solutions."
+          key="description"
+        />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-817476777"></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-817476777');
+        `}
+        </script>
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-817476777"
+        />
+        {/* pagesense */}
+        <script src="https://cdn-in.pagesense.io/js/satechsoftwareipvtltd/45f7f983729c4e3cb6b6f24aeed1c238.js"></script>
+        {/* End pagesense */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: ` window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-817476777'); `,
+          }}
+        />
+      </Head>
+
+      <main id="content" role="main" className="overflow-hidden">
+        <div
+          id="navbar"
+          className="navbar-area navbar-area-with-position-relative">
+          <div className="main-nav">
+            <div className="container">
+              <nav className="navbar navbar-expand-md navbar-light">
+                <Link href="/" className="navbar-brand">
+                  <img
+                    src="/images/sa-logo.svg"
+                    className="main-logo"
+                    alt="logo"
+                    width={80}
+                  />
+                  <img
+                    src="/images/sa-logo.svg"
+                    className="optional-logo"
+                    alt="logo"
+                    height={30}
+                    width={80}
+                  />
+                </Link>
+
+                <div className="cmmi">
+                  <h4>
+                    {" "}
+                    A CMMI Level <span>5</span> Company{" "}
+                  </h4>
+                </div>
+
+                <div className="collapse navbar-collapse mean-menu">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <span>Phone:</span>{" "}
+                      <a href="tel:+917058222899"> +91 7058 22 2899</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <div className="ml-main-banner-area">
+          <div className="container-fluid">
+            <div className="row align-items-center">
+              <div className="col-lg-6 col-md-12">
+                <div className="ml-main-banner-content">
+                  <h1>
+                    Google Cloud Platform Offer for{" "}
+                    <span>Small and Medium</span> Size Business
+                  </h1>
+                  <p>
+                    Empower your business with advance Google Cloud solutions.
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-lg-6 col-md-12">
+                <div className="ml-main-banner-image">
+                  <img
+                    src="https://dev1.satincorp.com/buy-google-workspace/wp-content/themes/buy-google-workspace-wp-themes-19032021/assets/img/banners/Google-cloud-partners-new.png"
+                    alt="image"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ai-main-banner-shape1">
+            <img src="/images/home-seven/banner/shape1.png" alt="image" />
+          </div>
+          <div className="ai-main-banner-shape2">
+            <img src="/images/home-seven/banner/shape2.png" alt="image" />
+          </div>
+          <div className="ai-main-banner-shape3">
+            <img src="/images/home-seven/banner/shape4.png" alt="image" />
+          </div>
+        </div>
+      </main>
+
+      <section className="contactPageSupport bg-f2f6f9 pt-100">
+        <div className="container">
+          <div className="section-title with-underline-text">
+            <h2>Contact Us</h2>
+            <h4 style={{ marginTop: "20px" }}>
+              Simplifying Your Reach: Expert Help Just a Click Away.
+            </h4>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-sm-3">
+              <div className="services-area">
+                <div className="single-services-box">
+                  <div className="icon">
+                    <img
+                      src="/images/icons/indusrty-icon/calender-sa.jpg"
+                      alt="image"
+                      style={{
+                        width: "50%",
+                        textAlign: "center",
+                        marginTop: "10px",
+                      }}
+                    />
+                  </div>
+                  <h2 style={{ margin: "20px 0" }}>Book a meeting</h2>
+                  <div className="btn-box" style={{ marginTop: "30px" }}>
+                    <Link
+                      href="https://crm.zoho.in/bookings/15minutesmeeting?rid=83ae67e835471568d742ef9582b4ef33b8f36dd0fb7cdebd602c8c357a84e3008ff2bddb5ab17b32e4f832c99492f2efgide6130e6203d90c8a72b192686e28ff7b9451257b4d55de887183f3d06b9d34a1"
+                      className="btn btn-primary">
+                      Book Now
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-3">
+              <div className="services-area">
+                <div className="single-services-box">
+                  <div className="icon">
+                    <img
+                      src="/images/icons/indusrty-icon/chat.png"
+                      alt="image"
+                      style={{
+                        width: "70%",
+                        textAlign: "center",
+                        marginTop: "10px",
+                      }}
+                    />
+                  </div>
+                  <h2 style={{ margin: "10px 0" }}>Chat with us</h2>
+                  <p>
+                    Get expert IT advice now. Chat with our specialists for
+                    solutions and guidance
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-sm-3">
+              <div className="services-area">
+                <div className="single-services-box">
+                  <div className="icon">
+                    <img
+                      src="/images/icons/indusrty-icon/Whatsapp-icon.png"
+                      alt="image"
+                      style={{
+                        width: "70%",
+                        textAlign: "center",
+                        marginTop: "10px",
+                      }}
+                    />
+                  </div>
+                  <h2 style={{ margin: "11px 0" }}>Chat with WhatsApp</h2>
+                  <div className="btn-box" style={{ marginTop: "10px" }}>
+                    <a
+                      href="https://api.whatsapp.com/send/?phone=%2B9107058188896&text=Hello!%20I%27m%20interested%20in%20exploring%20your%20SAT%20Services.%20Can%20you%20share%20more%20details?"
+                      className="btn btn-primary"
+                      style={{
+                        width: "15%",
+                        padding: "7px",
+                        borderRadius: "50%",
+                      }}>
+                      <img
+                        src="/images/icons/indusrty-icon/Whatsapp-icon-1.png"
+                        alt="image"
+                        style={{
+                          textAlign: "center",
+                        }}
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="services-area">
+                <div className="single-services-box">
+                  <div className="icon">
+                    <img
+                      src="/images/icons/indusrty-icon/call.png"
+                      alt="image"
+                      style={{
+                        width: "50%",
+                        textAlign: "center",
+                        marginTop: "10px",
+                      }}
+                    />
+                  </div>
+                  <h2 style={{ margin: "34px 0" }}>Call us now India</h2>
+                  <h3 style={{ marginTop: "40px" }}>
+                    {/* <a
+                      href="https://api.whatsapp.com/send/?phone=%2B9107058188896&text=Hello!%20I%27m%20interested%20in%20exploring%20your%20SAT%20Services.%20Can%20you%20share%20more%20details?"
+                      style={{ textDecoration: "2px dashed underline" }}>
+                      +91 7058222899
+                    </a> */}
+                    <a
+                      href="tel:(+91) -7058222899"
+                      style={{ textDecoration: "2px dashed underline" }}>
+                      +91 7058222899
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <AboutAI />
+
+      <TopFeaturedSolutions />
+
+      <CustomerSuccess />
+
+      <React.Fragment>
+        {useScript(
+          "https://salesiq.zoho.in/widget",
+          "siqd501e20f5ff83957af2415d36330a0344fef3b83965c959b579f4a9444020527"
+        )}
+      </React.Fragment>
+      <Link
+        href="https://api.whatsapp.com/send/?phone=%2B917058222899&text=Hello,%20I%20am%20interested%20in%20%20buying%20Google%20Cloud%20Platform"
+        target="_blank"
+        className="floatWhatsApp">
+        <img src="/images/icons/whatsApp.svg" alt="whatsapp" />
+      </Link>
+    </>
+  );
+};
+
+export default ContactFormContent;
