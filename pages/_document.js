96e8ac996dd21492f36a21a434470ca7b3f245c6
Sopan Ganage <gangesm1@gmail.com>
2024-05-03 14:20:05 +0530

    added apollo

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,18 @@ class MyDocument extends Document {
           />
           <link rel="icon" type="image/png" href="/favicon.png"></link>
           <link rel="icon" type="image/png" href="/180x180.png"></link>
-      
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+          o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+          o.onload=function(){window.trackingFunctions.onLoad({appId:"6631189a75a94c0438fce0f4"})},
+          document.head.appendChild(o)}initApollo();
+          `,
+            }}
+          />
+
           {/* ZOho Code 01/03/2024 */}
           <script
             dangerouslySetInnerHTML={{
