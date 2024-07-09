20df1e0ccca2b1b0ec9f42e0577e4e0498caf2c3
Sopan Ganage <gangesm1@gmail.com>
2024-07-09 21:41:45 +0530

    update navbar

--- pages/startups.js
+++ pages/startups.js
@@ -33,8 +33,8 @@ const Index = () => {
   return (
     <>
       <WebPageJsonLd
-        headline="Global Capability Center (GCC) Services in India | SA Technologies"
-        description="Explore SA Technologies GCC As a services for global expansion Maximize efficiency and innovation with our support"
+        headline="Scale Up Seamlessly: Build Your Offshore IT Team (100-500+) with SAT's Expertise"
+        description="Struggling to find top IT talent? Explore Latin America's tech powerhouses & build a skilled 100-500+ person offshore team with SAT's guidance. Cost-effective, nearshore solution for startups."
         author="SA Technologies"
         keywords="GCC As a services"
         image="https://www.satincorp.com/images/banners/gcc-1.jpg"
@@ -44,15 +44,15 @@ const Index = () => {
           type: "website",
           url: "https://www.satincorp.com/gcc/",
           title:
-            "Global Capability Center (GCC) Services in India | SA Technologies",
+            "Scale Up Seamlessly: Build Your Offshore IT Team (100-500+) with SAT's Expertise",
           description:
-            "Explore SA Technologies GCC As a services for global expansion Maximize efficiency and innovation with our support",
+            "Struggling to find top IT talent? Explore Latin America's tech powerhouses & build a skilled 100-500+ person offshore team with SAT's guidance. Cost-effective, nearshore solution for startups.",
           images: [
             {
               url: "https://www.satincorp.com/images/banners/gcc-1.jpg",
               width: 800,
               height: 600,
-              alt: "Explore SA Technologies GCC As a services for global expansion Maximize efficiency and innovation with our support",
+              alt: "Struggling to find top IT talent? Explore Latin America's tech powerhouses & build a skilled 100-500+ person offshore team with SAT's guidance. Cost-effective, nearshore solution for startups.",
             },
           ],
           authorName: "SA Technologies",
