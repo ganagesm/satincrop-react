deb722a23185da3e572fdb74c9f045fa7d178bb5
Sopan Ganage <gangesm1@gmail.com>
2024-08-27 18:38:09 +0530

    address changes

--- components/Contact/Offices.js
+++ components/Contact/Offices.js
@@ -52,7 +52,7 @@ const Services = () => {
             <div className="col-lg-3 col-sm-6 col-md-6">
               <div
                 className="single-main-services-box"
-                style={{ height: "250px" }}>
+                style={{ height: "230px" }}>
                 <img
                   src="/images/location.svg"
                   alt="image"
@@ -63,23 +63,46 @@ const Services = () => {
                   }}
                 />
                 <h3>Pune</h3>
-                {/* <p>
-                  D-6030, Solitaire Business Hub, Viman Nagar, Pune, Maharashtra
-                  411014
-                </p> */}
                 <p>
                   B Core, Office No. 2010, Solitaire Business Hub, Viman Nagar,
                   Pune
                 </p>
-                <p>
+                {/* <p>
+                  D-6030, Solitaire Business Hub, Viman Nagar, Pune, Maharashtra
+                  411014
+                </p> */}
+                {/* <p>
                   Phone: <a href="tel:+91 7823 02 5808">+91 7823 02 5808</a>
-                </p>
+                </p> */}
               </div>
             </div>
-            <div className="col-lg-4 col-sm-6 col-md-6">
+            <div className="col-lg-3 col-sm-6 col-md-6">
               <div
                 className="single-main-services-box"
-                style={{ height: "250px" }}>
+                style={{ height: "230px" }}>
+                <img
+                  src="/images/location.svg"
+                  alt="image"
+                  style={{
+                    width: "15%",
+                    textAlign: "center",
+                    marginTop: "10px",
+                  }}
+                />
+                <h3>Pune</h3>
+                <p>
+                  D-6030, Solitaire Business Hub, Viman Nagar, Pune, Maharashtra
+                  411014
+                </p>
+                {/* <p>
+                  Phone: <a href="tel:+91 7823 02 5808">+91 7823 02 5808</a>
+                </p> */}
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6 col-md-6">
+              <div
+                className="single-main-services-box"
+                style={{ height: "230px" }}>
                 <img
                   src="/images/location.svg"
                   alt="image"
@@ -90,23 +113,23 @@ const Services = () => {
                   }}
                 />
                 <h3>Bangalore</h3>
-                <p>
-                  2nd floor, Tower A1, Golden Enclave HAL old Airport Road,
-                  Bangalore - 560008
-                </p>
                 {/* <p>
                   613/A, 2nd Floor,15th Cross Ring Road, 33rd Main, J P nagar
                   1st Phase, Bangalore – 560078
                 </p> */}
                 <p>
-                  Phone: <a href="tel:080 4375 1188">080 4375 1188</a>
+                  2nd floor, Tower A1, Golden Enclave HAL old Airport Road,
+                  Bangalore -560008
                 </p>
+                {/* <p>
+                  Phone: <a href="tel:080 4375 1188">080 4375 1188</a>
+                </p> */}
               </div>
             </div>
             {/* <div className="col-lg-3 col-sm-6 col-md-6">
               <div
                 className="single-main-services-box"
-                style={{ height: "250px" }}>
+                style={{ height: "230px" }}>
                 <img
                   src="/images/location.svg"
                   alt="image"
@@ -121,13 +144,13 @@ const Services = () => {
                   Awfis Office, Winway World Offices Plot No. B-7, Vijay Nagar,
                   Indore, Madhya Pradesh, 452 010. India
                 </p>
-                 <p>Phone: <a href="tel:080 4375 1188">080 4375 1188</a></p>
+                <p>Phone: <a href="tel:080 4375 1188">080 4375 1188</a></p>
               </div>
             </div> */}
             <div className="col-lg-3 col-sm-6 col-md-6">
               <div
                 className="single-main-services-box"
-                style={{ height: "250px" }}>
+                style={{ height: "230px" }}>
                 <img
                   src="/images/location.svg"
                   alt="image"
