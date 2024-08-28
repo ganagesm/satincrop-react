9c056cccbed910bea7097b3bd0f66b947aa6ebee
Sopan Ganage <gangesm1@gmail.com>
2024-08-28 22:13:40 +0530

    client logo

--- components/Live/PartnerContent.js
+++ components/Live/PartnerContent.js
@@ -19,6 +19,169 @@ const PartnerContent = () => {
               cultures ensures our engineers align perfectly with your approach.{" "}
             </p>
           </div>
+          {/* <div className="customers-partner-list">
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="100">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/eehf8ksxg75kejjlwhe3"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="200">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/on3ifrv6es7nltw8dvm3"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="300">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/qks3nddcghydzkkmxyp1"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="400">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/aeetodqfmpm5a4vatpty"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="500">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/di99xtvny4pqx6pl0kpr"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="600">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/utqzw1w5hnralajdioiw"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="700">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/zxeqwcafer5nzjzklyxg"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="800">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/luxqcsfmp64kw2e2czhq"
+                  alt="image"
+                />
+              </a>
+            </div>
+
+            <div
+              className="partner-item"
+              data-aos="fade-in"
+              data-aos-duration="1200"
+              data-aos-delay="900">
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+                rel="noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dpoxq3jcb/image/upload/f_auto,q_auto/v1/next/clients/f9ersisojutxg5dtecfo"
+                  alt="image"
+                />
+              </a>
+            </div>
+          </div> */}
           <div className="customers-partner-list">
             <div
               className="partner-item"
@@ -181,16 +344,6 @@ const PartnerContent = () => {
                 />
               </a>
             </div>
-
-            {/* <div className="col-lg-12 col-md-12">
-              <div className="ai-all-services-btn">
-                <Link href="/customers-and-partners/" className="btn btn-primary">
-                  Discover More
-                </Link>
-              </div>
-            </div> */}
-          </div>
-          <div className="customers-partner-list">
             <div
               className="partner-item"
               data-aos="fade-in"
